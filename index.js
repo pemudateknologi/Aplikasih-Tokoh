f1e09dc6867bc1d7348716989d08f6a6d4694371
pemudateknologi <achferdihamdani@gmail.com>
2024-06-05 16:03:34 +0700

    membuat aplikasih tokoh"

---
+++ index.js
@@ -0,0 +1,27 @@
+/** @format */
+
+/** @format */
+
+const btnPopup = document.getElementById("btn-popup");
+const popup = document.querySelector(".popup");
+const delet = document.querySelector("a");
+const body = document.body;
+const containerPopup = document.querySelector(".container-popup");
+const settins = document.querySelector(".settings a");
+const slider = document.querySelector(".slider");
+const humbuger = document.querySelector(".logo a");
+
+humbuger.addEventListener("click", function () {
+  slider.classList.toggle("active");
+});
+
+btnPopup.addEventListener("click", function () {
+  popup.remove("popup");
+  containerPopup.remove("container-popup");
+});
+delet.addEventListener("click", function () {
+  body.remove("body");
+});
+settins.addEventListener("click", function () {
+  settins.classList.toggle("active");
+});
